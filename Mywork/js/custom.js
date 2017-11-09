5efcc2079f3d3cb986edbfcee77e6a53760972a8
Minaxmi <minaxmi.saha@gmail.com>
2017-11-09 07:45:41 +0600

    Mywork

---
+++ Mywork/js/custom.js
@@ -0,0 +1,5 @@
+jQuery(window).load(function() { 
+      $('#status').fadeOut(10000);
+      $('#preloader').delay(5000 ).fadeOut('slow');
+      $('body').delay(5000).css({'overflow':'visible'});
+    })
